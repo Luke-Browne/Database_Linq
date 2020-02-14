98b8625205cd0ca29d577a88cf5538161c9549c7
Luke Browne <S00187306@mail.itsligo.ie>
2020-02-14 10:14:58 +0000

    Ex1, 2 and 3

--- Database_Linq/MainWindow.xaml.cs
+++ Database_Linq/MainWindow.xaml.cs
@@ -27,12 +27,41 @@ namespace Database_Linq
             InitializeComponent();
         }
 
+
+        // Ex1 Customer Names
         private void btnQueryEx1_Click(object sender, RoutedEventArgs e)
         {
             var query = from c in db.Customers
                         select c.CompanyName;
 
             lbxCustomersEx1.ItemsSource = query.ToList();
+        }
+
+        // Ex2 Customer Objects
+        private void btnQueryEx2_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c;
+
+            dgCustomersEx2.ItemsSource = query.ToList();
+        }
+
+        // Ex3 Order Information
+        private void btnQueryEx3_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from o in db.Orders
+                        where o.Customer.City.Equals("London")
+                        || o.Customer.City.Equals("Paris")
+                        || o.Customer.Country.Equals("USA")
+                        orderby o.Customer.CompanyName
+                        select new
+                        {
+                            CustomerName = o.Customer.CompanyName,
+                            City = o.Customer.City,
+                            Address = o.ShipAddress
+                        };
+
+            dgCustomersEx3.ItemsSource = query.ToList().Distinct();
         }
     }
 }
